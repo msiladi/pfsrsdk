b13fccb5a6a7728b46c4a048bb56a7d736f01411
Adam Wheeler <ajwtech@gmail.com>
2018-08-17 15:09:34 -0400

    Updated Git ignores and began using packrat updated test_auth to read multiple environments to test compatibility between versions. Replaced for loop with apply

--- tests/testthat/test_auth.R
+++ tests/testthat/test_auth.R
@@ -2,97 +2,45 @@
 context("Tests for authentication")
 
 
-
 rm(list=ls())
 
-verbose <- FALSE
-
-
+verbose <- TRUE
 #setup to test against multiple environments
+#name files Auth-[pfsversion].json example Auth-5.3.8 for this to pick them up. 
+environments<<-list.files("test_environment","^(Auth-)[0-9]+\\.[0-9]+\\.[0-9]+\\.json$",full.names=TRUE)
 
-environments<<-list.files("test_environments","5-3-8.json",full.names=TRUE)
+cat(paste0("\n environments:\n",environments,"\n"))
 
-print(environments)
-for (i in 1:length(environments))
-  {
+lapply(environments, function(x){
 
-
-
-     test_that(paste("test login parameters for environment", environments[i]),
+     test_that(paste("test login parameters for environment and updating metadata", x),
             {
               verbose <- FALSE
-             
-              api <- CoreAPIV2::coreAPI(environments[i])
-
-          
-              con<- CoreAPIV2::authBasic(api,useVerbose=verbose)
-        
-              expect_match(api$coreUrl,con$coreApi$coreUrl,all=verbose)
-              expect_that(is.null(con$coreApi$jsessionId),equals(FALSE))
-              
-              
-              
-              
-              
-              logout<-CoreAPIV2::logOut(api,useVerbose = verbose)
-              expect_match(logout$success,"Success")
-
-              })
-  
-  
-  
-  test_that("single account with bad password returns error",
-                       {
-                         verbose <- FALSE
-                         api <- CoreAPIV2::coreAPI(environments[i])
-                        bapi<- api
-                        bapi$pwd <-"badpassword"
-                        expect_error(CoreAPIV2::authBasic(bapi,useVerbose = verbose))
-                       
-               })
-             
-
-
-}
-
-
-
-
-
-
-context("Tests metadata update")
-
-
-
-rm(list=ls())
-
-verbose <- FALSE
-
-
-#setup to test against multiple environments
-
-instance<<-"test_environments/5-2-2.json"
-
-  
-  
-  test_that(paste("test metadata update", instance),
-            {
-              verbose <- FALSE
-              
-              api <- CoreAPIV2::coreAPI(instance)
-              
-
+              api <- CoreAPIV2::coreAPI(x)
               con<- CoreAPIV2::authBasic(api,useVerbose=verbose)
               
               metadata<- CoreAPIV2::updateMetadata(con$coreApi,useVerbose=TRUE)
               print(httr::http_status(metadata$response))
               expect_match(httr::http_status(metadata$response)$category ,"Success" )
               
+              expect_match(api$coreUrl,con$coreApi$coreUrl,all=verbose)
+              expect_that(is.null(con$coreApi$jsessionId),equals(FALSE))
               logout<-CoreAPIV2::logOut(api,useVerbose = verbose)
               expect_match(logout$success,"Success")
-              
-            })
+
+              })
   
+  test_that("single account with bad password returns error",
+                       {
+                         verbose <- FALSE
+                         api <- CoreAPIV2::coreAPI(x)
+                        bapi<- api
+                        bapi$pwd <-"badpassword"
+                        expect_error(CoreAPIV2::authBasic(bapi,useVerbose = verbose))
+               })
+}
+)
+
 
 
 
