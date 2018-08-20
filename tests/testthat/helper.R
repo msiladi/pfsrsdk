b49877f5a9b37ad7114371cf9c5b5e1f17eca14c
Adam Wheeler <ajwtech@gmail.com>
2018-08-20 09:45:01 -0400

    Created a helper function. Variables need to be converted to options. This hold a basic connect function as well for simplifying testing. This will also enable multi version compatibility testing. Created a basicfunctions test to test the get post and put functions on a POCO. Added an xml file for import to PFS to enable testing. Rewrote test_auth to remove for loops and enable applying testing against multiple systems simultaniously. initiated a nameing convention for json testing files. The file must start with "Auth-" and then have a three part version number separated by "." to meet the regex.
    Tested running auth and basic functions against 5.3.8 and 6.0.0

---
+++ tests/testthat/helper.R
@@ -0,0 +1,40 @@
+
+
+
+verbose <- TRUE
+#setup to test against multiple environments
+#name files Auth-[pfsversion].json example Auth-5.3.8 for this to pick them up.
+
+
+environments<-list.files("test_environment","^(Auth-)[0-9]+\\.[0-9]+\\.[0-9]+\\.json$",full.names=TRUE)
+
+## Set environments to be tested.
+if (length(environments) > 1){
+  #Offer the user the option to select the environments that they would like to test.
+  cat("Choose the environments that you want to test from the list below. separate each number with a space. hit return to test all\n")
+  print.simple.list(environments)
+  selection <- readline()
+  if(nchar(selection) > 0) environments <- environments[lapply(strsplit(selection, " "), as.numeric)[[1]]]
+}
+
+POCO <- "POCO"
+
+Connect <- function(envString){
+  api <- CoreAPIV2::coreAPI(envString)
+  con<- CoreAPIV2::authBasic(api,useVerbose=verbose)
+}
+
+  
+  
+  
+  # })
+# 
+# testthat::teardown({
+#   
+#   POCO <- NULL
+#   EnvSelection <- NULL
+#   selection <-NULL
+#   environments <-NULL
+#   verbose <-NULL
+#   rm(list=ls())
+# })
--- tests/testthat/test_auth.R
+++ tests/testthat/test_auth.R
@@ -2,33 +2,28 @@
 context("Tests for authentication")
 
 
-rm(list=ls())
-
-verbose <- TRUE
-#setup to test against multiple environments
-#name files Auth-[pfsversion].json example Auth-5.3.8 for this to pick them up. 
-environments<<-list.files("test_environment","^(Auth-)[0-9]+\\.[0-9]+\\.[0-9]+\\.json$",full.names=TRUE)
-
 cat(paste0("\n environments:\n",environments,"\n"))
 
 lapply(environments, function(x){
-
-     test_that(paste("test login parameters for environment and updating metadata", x),
+   con <- Connect(x)
+   test_that(paste("test login parameters for environment and updating metadata", x),
+          {
+            expect_that(is.null(con$coreApi$jsessionId),equals(FALSE))
+          })
+   
+  test_that(paste("test Updating MetaData of: ", x),
             {
-              verbose <- FALSE
-              api <- CoreAPIV2::coreAPI(x)
-              con<- CoreAPIV2::authBasic(api,useVerbose=verbose)
-              
               metadata<- CoreAPIV2::updateMetadata(con$coreApi,useVerbose=TRUE)
               print(httr::http_status(metadata$response))
               expect_match(httr::http_status(metadata$response)$category ,"Success" )
               
-              expect_match(api$coreUrl,con$coreApi$coreUrl,all=verbose)
-              expect_that(is.null(con$coreApi$jsessionId),equals(FALSE))
-              logout<-CoreAPIV2::logOut(api,useVerbose = verbose)
+            })
+  test_that(paste("test logout of: ", x),
+            {
+              logout<-CoreAPIV2::logOut(con$coreApi,useVerbose = verbose)
               expect_match(logout$success,"Success")
-
-              })
+              
+            })
   
   test_that("single account with bad password returns error",
                        {
