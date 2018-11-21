1b76973d4edc33e49fd13207fd9f8215882faa68
Adam Wheeler <ajwtech@gmail.com>
2018-11-21 14:15:30 -0500

    Mostly test updates General clean up in apiPUT.r Expanded helper for dynamic testing broke test create out into several test files Created new basicpoco xml. The previous one breaks functionality in PFS because of objects having the same name.

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -10,6 +10,7 @@ environments <- list.files("test_environment", "^(Auth-)[0-9]+\\.[0-9]+\\.[0-9]+
 
 ## Set environments to be tested.
 if (length(environments) > 1) {
+
   # Offer the user the option to select the environments that they would like to test.
   cat("Choose the environments that you want to test from the list below. separate each number with a space. hit return to test all\n")
   print.simple.list(environments)
@@ -17,7 +18,15 @@ if (length(environments) > 1) {
   if (nchar(selection) > 0) environments <- environments[lapply(strsplit(selection, " "), as.numeric)[[1]]]
 }
 
-POCO <- "POCO"
+
+POCO60NAME <- "TE1"
+TESTPOCO <- "TESTENTITY"
+POCOASSOC <- "TESTASSOC"
+POCOASSOC1NAME <- "TA1"
+POCOASSOC2NAME <- "TA2"
+ASSOCIATIONCONTEXT <- "TEST"
+ASSOCIATIONCONTEXTLISTNAME <- "TEST"
+
 
 Connect <- function(envString) {
   api <- CoreAPIV2::coreAPI(envString)
