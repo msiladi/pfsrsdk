2e63f95e58624e12bf335674a7764721e757eeb9
Adam Wheeler <ajwtech@gmail.com>
2018-11-29 09:20:11 -0500

    Created getEntityMetadata test

---
+++ tests/testthat/test-getEntityMetadata.R
@@ -0,0 +1,18 @@
+
+context("Tests for getEntityMetadata")
+
+# Completed regression for 5.3.8 and 6.0.1
+
+lapply(environments, function(x) {
+  
+  con <- Connect(x)
+  test_that(paste("test getEntityMetadata() on: ", x), {
+    out <- CoreAPIV2::getEntityMetadata(con$coreApi, TESTPOCO, useVerbose = FALSE)
+   
+    #Check to verify that the function returned a list. dependending on tenent this list may be empty
+    expect_equivalent(class(out$template), "list", all = verbose)
+    
+  })
+  
+  CoreAPIV2::logOut(con$coreApi)
+})
