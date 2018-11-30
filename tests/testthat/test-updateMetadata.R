cf2c89e035d07a3638562713c9ad3640292c569f
Adam Wheeler <adam.j.wheeler@accenture.com>
2018-11-30 14:27:16 +0000

    test-updateMetadata.R edited online with Bitbucket Fixed Typo

--- tests/testthat/test-updateMetadata.R
+++ tests/testthat/test-updateMetadata.R
@@ -1,12 +1,12 @@
 #' @author Adam Wheeler adam.j.wheeler@accenture.com
-#' @description \code Tests for updatemetadata
+#' @description \code Tests for updateMetadata
 
-context("test-updatemetadata")
+context("test-updateMetadata")
 
 lapply(environments, function(x) {
   
   con <- Connect(x)
-  test_that(paste("test updatemetadata() on: ", x), {
+  test_that(paste("test updateMetadata() on: ", x), {
     meta <- updateMetadata(con$coreApi, useVerbose = verbose)
     expect_equivalent(meta$response$status_code, 200, all = verbose)
   })
