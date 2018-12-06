581cb96502670045978e12fe25bf9adce43e531b
Adam Wheeler <ajwtech@gmail.com>
2018-12-06 10:05:00 -0500

    Deprecated our first function.
    Added CoreAPIV2-derecated.r
    renamed ODATAcleanName.r to odataCleanName.r
    copied and deprecated the function in favor of the correctly named function.
    Moved the old tests with underscores to a separate file until we are ready to package and release. They should be deleted by then. There is a separate ticket for this.

---
+++ R/CoreAPIV2-deprecated.r
@@ -0,0 +1,9 @@
+## CoreAPIV2-deprecated.r
+#' @title Deprecated functions in package \pkg{CoreAPIV2}.
+#' @description The functions listed below are deprecated and will be defunct in
+#'   the near future. When possible, alternative functions with similar
+#'   functionality are also mentioned. Help pages for deprecated functions are
+#'   available at \code{help("-deprecated")}.
+#' @name CoreAPIV2-deprecated
+#' @keywords internal
+NULL
