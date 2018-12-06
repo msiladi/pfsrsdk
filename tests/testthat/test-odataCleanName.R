75335d5cf5823b99ac222f7ecffe761437c9a6d6
Adam Wheeler <ajwtech@gmail.com>
2018-12-06 11:40:35 -0500

    Fixed odata name and changed text so that it is not misleading.

--- tests/testthat/test-odataCleanName.R
+++ tests/testthat/test-odataCleanName.R
@@ -1,14 +1,14 @@
 #' @author Adam Wheeler adam.j.wheeler@accenture.com
-#' @description \code Test for ODATAcleanName.
+#' @description \code Test for odataCleanName.
 
-context("Tests for ODATAcleanName")
+context("Tests for odataCleanName")
 
 # Completed regression for 5.3.8 and 6.0.1
 
 
 
-test_that(paste("ODATAcleanName() replaces spaces with '_' and converts everything to uppercase "), {
-  CleanedUp <- CoreAPIV2::odataCleanName("1conVert me_toUPPERcase and make spaces underscores What about numbers 123453 32 _ and symbols!@#$%^&*()_+ -=`~ ")
+test_that(paste("odataCleanName() replaces spaces with '_' and converts everything to uppercase "), {
+  CleanedUp <- CoreAPIV2::odataCleanName("1 make spaces underscores What about numbers 123453 32 _ and symbols!@#$%^&*()_+ -=`~ ")
 
-  expect_equivalent(CleanedUp, "_1conVert_me_toUPPERcase_and_make_spaces_underscores_What_about_numbers_123453_32___and_symbols!@#$%^&*()_+_-=`~_")
+  expect_equivalent(CleanedUp, "_1_make_spaces_underscores_What_about_numbers_123453_32___and_symbols!@#$%^&*()_+_-=`~_")
 })
