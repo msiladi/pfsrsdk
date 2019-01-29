9964b35322b0bb3f6a3cfea035986a0b1f34763c
natashamora <natasha.mora@thermofisher.com>
2019-01-28 19:24:09 -0500

    Updated "account" to "tenant", "coreUrl" to "host", "user" to "username" and "pwd" to "password" in R/ files. Added author on R/authBasic.R

--- R/coreAPI.R
+++ R/coreAPI.R
@@ -1,16 +1,16 @@
 #' coreAPI Creates a object of class coreAPI that contains user and connection information.
-#' @param CoreAccountInfo file with account information in json format.
+#' @param CoreAccountInfo file with tenant information in json format.
 #' @return Object of class coreAPI
 #' @export
 #' @examples
 #' \dontrun{
 #' api<-CoreAPIV2::coreAPI("/home/environment.json")
 #' }
-#' @details{ Creates a object of class coreAPI that contains user name,
-#'          password base url, account, port.
-#'          It has slots for account, jsessionId, AWSELB, and base URL.
+#' @details{ Creates a object of class coreAPI that contains username,
+#'          password, base url, tenant, port.
+#'          It has slots for tenant, jsessionId, AWSELB, and base URL.
 #'          Requires a json file that is a POSTMAN environment file.
-#'         \code{#'Creates a object of class coreAPI that contains account information}
+#'         \code{#'Creates a object of class coreAPI that contains tenant information}
 #'         \code{coreAPI("path to json")}.
 #' }
 #' The json must include the fields shown below.
@@ -55,7 +55,7 @@
 #' }
 #'     }
 #'
-#'  The account value may be set to "" if the user only has access to one tenant.
+#'  The tenant value may be set to "" if the user only has access to one tenant.
 #' As an alternative the environment json object from Postman can be used directly.
 #' @author Craig Parman ngsAnalytics, ngsanalytics.com
 #' @author Scott Russell scott.russell@thermofisher.com
@@ -64,12 +64,12 @@ coreAPI <- function(CoreAccountInfo) {
   accountinfo <- jsonlite::fromJSON(CoreAccountInfo)$values
   structure(
     list(
-      user = getAccountInfoValue(accountinfo, "username"),
-      pwd = getAccountInfoValue(accountinfo, "password"),
-      account = getAccountInfoValue(accountinfo, "tenant"),
+      username = getAccountInfoValue(accountinfo, "username"),
+      password = getAccountInfoValue(accountinfo, "password"),
+      tenant = getAccountInfoValue(accountinfo, "tenant"),
       alias = getAccountInfoValue(accountinfo, "alias"),
       context = getAccountInfoValue(accountinfo, "context"),
-      coreUrl = getAccountInfoValue(accountinfo, "host"),
+      host = getAccountInfoValue(accountinfo, "host"),
       port = getAccountInfoValue(accountinfo, "port"),
       scheme = getAccountInfoValue(accountinfo, "scheme"),
       jsessionId = NULL,
