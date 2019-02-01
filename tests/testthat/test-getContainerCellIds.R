80f711feee26bcbf574eaebdc9391eac126ef2bd
Scott Russell <srussell@coreinformatics.com>
2019-02-01 17:03:30 -0500

    RSDK-39 Added tests compatilibity switch for getContainerCellIds

---
+++ tests/testthat/test-getContainerCellIds.R
@@ -0,0 +1,14 @@
+#' @author Scott Russell scott.russell@thermofisher.com
+#' @description \code Tests for getContainerCellIds.
+
+context("Tests for getContainerCellIds")
+
+# Completed regression for 5.3.8 and 6.0.1
+
+test_that(paste("test getContainerCellIds() on:", env$auth), {
+  result <- CoreAPIV2::getContainerCellIds(con$coreApi, data$containerType, data$containerBarcode, useVerbose = verbose)
+
+  expect_equal(result$response$status_code, 200)
+
+  expect_gt(unlist(length(result$entity)), 0)
+})
