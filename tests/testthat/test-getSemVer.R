6288d04041b9732789aaa1d4c46c3206eafb8d28
natashamora <natasha.mora@thermofisher.com>
2019-02-01 16:29:32 -0500

    Updated RSDK-35 and RSDK-92. Updated setSemVer to getSemVer. Updated "version" in createSampleLot with "dataBind". Updated test-getSemVer with expect_match() instead of expect(). Updated sampleBarcode in Data-5.3.8.json to be consistent with Data-.6.0.1.json.

---
+++ tests/testthat/test-getSemVer.R
@@ -0,0 +1,13 @@
+#' @author Natasha Mora natasha.mora@thermofisher.com
+#' @description \code Tests for getting PFS's Semantic Version.
+context("Tests for getSemVer")
+
+test_that(paste("test getSemVer on:", env$auth), {
+  #Tests if getSemVer is able to retrieve the PFS SemVer 
+  result <- CoreAPIV2::getSemVer(con$coreApi)
+  expect_match(result, "[0-9]+\\.[0-9]+\\.[0-9]+")
+
+  #Tests if the warning is returned when the server value in the Auth-*.json is NULL
+  con$coreApi$semVer <- NULL
+  expect_warning(CoreAPIV2::authBasic(con$coreApi),"SemVer variable")
+})
