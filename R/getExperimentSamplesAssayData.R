d6988019f60b5de3aa19986a4645fa86e465b19e
natashamora <natasha.mora@thermofisher.com>
2019-02-06 18:29:03 -0500

    Created test for getExperimentSamplesAssayData Added variables assayType and experimentSampleBarcode. Changed experimentSamblebarcode to experimentSampleBarcode. Renamed the function from getExperimentSampleAssayData to getExperimentSamplesAssayData. Added author. Updated man/ files with devtools::document() Updated format to modified test/* and R/files with styler.

--- R/getExperimentSamplesAssayData.R
+++ R/getExperimentSamplesAssayData.R
@@ -4,18 +4,19 @@
 #'
 #' @param coreApi coreApi object with valid jsessionid
 #' @param assayType assay type to get
-#' @param experimentSamplebarcode experiment sample barcode of entity to get
+#' @param experimentSampleBarcode experiment sample barcode of entity to get
 #' @param useVerbose TRUE or FALSE to indicate if verbose options should be used in http POST
 #' @return returns a list $entity contains entity information, $response contains the entire http response
 #' @export
 #' @examples
 #' \dontrun{
-#' api<-CoreAPIV2::CoreAPI("PATH TO JSON FILE")
-#' login<- CoreAPIV2::authBasic(api)
-#' experiment<-  getExperimentSamplesAssayData (login$coreApi,"assayType","barcode")
+#' api <- CoreAPIV2::CoreAPI("PATH TO JSON FILE")
+#' login <- CoreAPIV2::authBasic(api)
+#' experiment <- getExperimentSamplesAssayData(login$coreApi, "assayType", "barcode")
 #' CoreAPIV2:logOut(login$coreApi)
 #' }
 #' @author Craig Parman ngsAnalytics, ngsanalytics.com
+#' @author Natasha Mora natasha.mora@thermofisher.com
 #' @description \code{ getExperimentSamplesAssayData }  Gets experiment samples from experiment identified by experiment barcode.
 #' Does not retieve files attached as data. Use getExperimentSampleAssayFileData to retrieve assay data that is a file.
 
@@ -26,17 +27,17 @@
 getExperimentSamplesAssayData <-
   function(coreApi,
              assayType,
-             experimentSamplebarcode,
+             experimentSampleBarcode,
              useVerbose = FALSE) {
     # clean the name for ODATA
 
-    resource <- CoreAPIV2::ODATAcleanName("EXPERIMENT_SAMPLE")
+    resource <- CoreAPIV2::odataCleanName("EXPERIMENT_SAMPLE")
 
-    assayType <- CoreAPIV2::ODATAcleanName(assayType)
+    assayType <- CoreAPIV2::odataCleanName(assayType)
 
     query <- paste0(
       "('",
-      experimentSamplebarcode,
+      experimentSampleBarcode,
       "')/ASSAY_DATA/pfs.",
       assayType,
       "_DATA"
