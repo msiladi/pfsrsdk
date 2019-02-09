21a669c9f46d0ec1fee2fc22d933ce2464e2406d
natashamora <natasha.mora@thermofisher.com>
2019-02-08 19:02:48 -0500

    Created test for getExperimentSamplesRawData. Simplified tests for getExperimentSamples and getExperimentSampleAssayData. Add variable to the Data-*.json files.

---
+++ tests/testthat/test-getExperimentSamplesRawData.R
@@ -0,0 +1,14 @@
+#' @author Natasha Mora natasha.mora@thermofisher.com
+#' @description \code Tests for getExperimentSamplesRawData.
+
+context("Tests for getExperimentSamplesRawData")
+
+# Completed regression for 5.3.8 and 6.0.1
+
+test_that(paste("test getExperimentSamplesRawData() on:", env$auth), {
+  result <- CoreAPIV2::getExperimentSamplesRawData(con$coreApi, data$experimentContainerBarcode, useVerbose = verbose)
+
+  expect_equal(result$response$status_code, 200)
+  
+  expect_gt(length(result$entity$DATA_VALUE), 0)
+})
