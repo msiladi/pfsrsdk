48cb6f4db8d7312dbe5ced0a4cd021eb8e82b651
natashamora <natasha.mora@thermofisher.com>
2019-02-11 17:08:01 -0500

    Created test for getExperimentContainers. Udtaed getExperimentContainers: Added author. Changed query logic to be compatible with 5.3.8 and 6.0.1. Renamed parameters. Ran styler. Man/ files updated by devtools::document()

--- R/getExperimentContainers.R
+++ R/getExperimentContainers.R
@@ -3,18 +3,19 @@
 #' \code{getExperimentContainers}  Gets experiment containers from experiment identified by barcode.
 #' @param coreApi coreApi object with valid jsessionid
 #' @param experimentType experiment entity type to get
-#' @param barcode barcode of experiment to query
+#' @param experimentBarcode barcode of experiment to query
 #' @param useVerbose TRUE or FALSE to indicate if verbose options should be used in http POST
 #' @return returns a list $entity contains barcodes of the containers, $response contains the entire http response
 #' @export
 #' @examples
 #' \dontrun{
-#' api<-CoreAPIV2::CoreAPI("PATH TO JSON FILE")
-#' login<- CoreAPIV2::authBasic(api)
-#' exptCaontainerBarcodes <-  CoreAPIV2::getExperimentContainers(login$coreApi,"entityType","barcode")
+#' api <- CoreAPIV2::CoreAPI("PATH TO JSON FILE")
+#' login <- CoreAPIV2::authBasic(api)
+#' exptCaontainerBarcodes <- CoreAPIV2::getExperimentContainers(login$coreApi, "experimentType", "experimentBarcode")
 #' CoreAPIV2:logOut(login$coreApi)
 #' }
 #' @author Craig Parman ngsAnalytics, ngsanalytics.com
+#' @author Natasha Mora natasha.mora@thermofisher.com
 #' @description \code{getExperimentContainers}  Gets experiment contaniers from experiment identified by experiment barcode.
 
 
@@ -23,24 +24,30 @@
 getExperimentContainers <-
   function(coreApi,
              experimentType,
-             barcode,
+             experimentBarcode,
              useVerbose = FALSE) {
     # clean the name for ODATA
 
-    resource <- CoreAPIV2::ODATAcleanName(experimentType)
+    resource <- CoreAPIV2::odataCleanName(experimentType)
 
 
-    query <-
-      paste0(
-        "('",
-        barcode,
-        "')/REV_CONTAINER_EXPERIMENT_EXPERIMENT_CONTAINER"
+    query <- switch(EXPR = substr(coreApi$semVer, 1, 1),
+      "2" = paste0(
+        "('", experimentBarcode, "')/REV_CONTAINER_EXPERIMENT_EXPERIMENT_CONTAINER"
+      ),
+      "3" = paste0(
+        "('", experimentBarcode, "')/EXPERIMENT_CONTAINERS"
+      ),
+      print(
+        paste0(
+          "('", experimentBarcode, "')/EXPERIMENT_CONTAINERS"
+        )
       )
+    )
 
 
     header <-
       c("Content-Type" = "application/json;odata.metadata=full", Accept = "application/json")
-
 
 
     response <-
@@ -51,8 +58,6 @@ getExperimentContainers <-
         headers = header,
         useVerbose = useVerbose
       )
-
-
 
 
     list(entity = unlist((
