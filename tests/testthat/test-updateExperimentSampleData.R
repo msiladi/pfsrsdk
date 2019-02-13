3aa83bac0e873c5aea9ac1c5b82950b00fb96ebb
natashamora <natasha.mora@thermofisher.com>
2019-02-12 23:13:07 -0500

    Addresed RSDK-62. Created test-updateExperimentSampleData file. Updated R/updateExperimentSampleData.R Added variable to Data-*.json files. Included unboxing to the assayAttributeValues variable. Skipped test-updateExperimentSampleData due to RSDK-100. Ran styler::style_file() and devtools::document().

---
+++ tests/testthat/test-updateExperimentSampleData.R
@@ -0,0 +1,16 @@
+#' @author Natasha Mora natasha.mora@thermofisher.com
+#' @description \code{updateExperimentSampleData} Tests for updateExperimentSampleData.
+
+context("Tests for updateExperimentSampleData")
+
+# Completed regression for 5.3.8 and 6.0.1
+
+test_that(paste("test updateExperimentSampleData() on: ", env$auth), {
+  skip("SDK command 'updateExperimentSampleData' fails on CI envs. See RSDK-100")
+
+  result <- CoreAPIV2::updateExperimentSampleData(con$coreApi, data$experimentAssayType, data$experimentSampleBarcode, jsonlite::unbox(data$experimentAssayUpdateAttrList), useVerbose = verbose)
+
+  expect_equal(result$response$status_code, 200)
+
+  expect_equal(result$entity[[names(data$experimentAssayUpdateAttrList)[1]]], data$experimentAssayUpdateAttrList[[1]])
+})
