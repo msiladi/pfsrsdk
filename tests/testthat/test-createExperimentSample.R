c1f5cde65994cb975c560e8d2f22dbc80018f3ea
Adam Wheeler <ajwtech@gmail.com>
2019-02-13 19:09:42 -0500

    fixes for RSDK-80, RSDK-98 and RSDK-34
    Fixed CreateExperimentSample
    Updated Teardown so the logout output isnt shown. it makes it so I have to scroll up to see my test results.
    
    Fixed CreateExperimentSample
    
    Corrected documentation
    
    Fixed CreateExperimentSample
    Added Scotts attempt at forcing the system to load the SDK xml.
    
    Installed CoreApp.SDK XML on the test systems and the issue was resolved.
    
    added loadXMLFunction unexported and experimental
    
    RSDK-80 update to loadXml and testthat/setup for loading SDK config

---
+++ tests/testthat/test-createExperimentSample.R
@@ -0,0 +1,14 @@
+#' @author Adam Wheeler adam.j.wheeler@accenture.com
+#' @description Tests for Experiment creation.
+#'
+context("Tests for createExperimentSample")
+
+test_that(paste("test createExperimentSample() on: ", env$auth), {
+
+  samp <- CoreAPIV2::createExperimentSample(con$coreApi,
+                                            data$experimentType,
+                                            data$experimentBarcode,
+                                            data$sampleLotBarcode)
+
+  expect_that(httr::http_status(samp$response)$reason, equals("Created"))
+})
