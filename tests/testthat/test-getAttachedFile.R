500189a5304306c2a25d48e6e291a6d1dfc70c6a
Adam Wheeler <ajwtech@gmail.com>
2019-02-18 15:28:15 -0500

    Changed name of getAttachedfile to be getAttachedAttributeFile and deprecated the former.
    Added Rlang back to the description.
    
    Changed name of getAttachedfile to be getAttachedAttributeFile and deprecated the former.

---
+++ tests/testthat/test-getAttachedFile.R
@@ -0,0 +1,34 @@
+#' @author Adam Wheeler adam.j.wheeler@accenture.com
+#' @author Scott Russell scott.russell@thermofisher.com
+#' @description Tests for attachFile.
+#'
+context("Tests for attachFile()")
+
+barcode <- CoreAPIV2::getEntityByName(con$coreApi, data$testPocoType, data$testPocoName, FALSE, FALSE)$entity[[1]]$Barcode
+filePath <- tempfile(fileext = ".csv")
+write.csv(x = runif(n = 1000), file = filePath)
+
+test_that(paste("test attachFile() OData call on:", env$auth), {
+  attachedFile <- CoreAPIV2::attachFile(
+    coreApi = con$coreApi,
+    entityType = data$testPocoType,
+    barcode = barcode,
+    filePath = filePath,
+    targetAttributeName = data$testPocoFileAttrName
+  )
+
+  expect_equivalent(attachedFile$response$status_code, 204, all = verbose)
+  expect_null(attachedFile$response$entity)
+})
+
+test_that(paste("test attachFile() CoreSDK call on:", env$auth), {
+  attachedFile <- CoreAPIV2::attachFile(
+    coreApi = con$coreApi,
+    entityType = data$testPocoType,
+    barcode = barcode,
+    filePath = filePath
+  )
+
+  expect_equivalent(attachedFile$response$status_code, 200, all = verbose)
+  expect_true(attachedFile$entity$response$success)
+})
