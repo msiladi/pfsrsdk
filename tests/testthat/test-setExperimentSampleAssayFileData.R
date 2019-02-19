33db40e83ffee03839078ad720ebd2ebe8796d0b
Scott Russell <srussell@coreinformatics.com>
2019-02-19 17:39:54 -0500

    RSDK-99 Updated setExperimentSampleAssayFileData for compatibility
    Added test for setExperimentSampleAssayFileData.R
    Deprecated setExperimentSamplesAssayFileData.R
    Updated stringr version in packrat

---
+++ tests/testthat/test-setExperimentSampleAssayFileData.R
@@ -0,0 +1,22 @@
+#' @author Scott Russell scott.russell@thermofisher.com
+#' @description \code Tests for setExperimentSampleAssayFileData
+
+context("Tests for setExperimentSampleAssayFileData")
+
+# Completed regression for 5.3.8 and 6.0.1
+
+filePath <- tempfile(fileext = ".csv")
+write.csv(x = runif(n = 1000), file = filePath)
+
+test_that(paste("test setExperimentSampleAssayFileData() on:", env$auth), {
+  result <- CoreAPIV2::setExperimentSampleAssayFileData(con$coreApi,
+    data$experimentFileAssayType,
+    data$experimentSampleBarcodeWithFileAttr,
+    data$fileAttrName,
+    filePath,
+    useVerbose = verbose
+  )
+
+  expect_equal(result$response$status_code, 204)
+  expect_null(result$response$entity)
+})
