5ca3da1f2c791e66b74c2f463bc59b415e00e99a
Francisco Marin <francisco.marin@thermofisher.com>
2019-05-02 17:13:39 -0400

    RSDK-79: pfsrsdk error handling on HTTP functions
    Changing error handling to print PFS error messages.
    Since the Stop function collapses the error result into a 1000 character string, I am building the error message returned by the function.
    Also, 3 files were added to support testing of the error handling.
    
    RSDK-79: pfsrsdk error handling on HTTP functions
    
    Changing error handling to print PFS error messages.
    Since the Stop function collapses the error result into a 1000 character string, I am building the error message returned by the function.
    Also, 3 files were added to support testing of the error handlings.
    
    RSDK-125 error handling on http functions (corelims/pfsrsdk)
    Applying feedback from Natasha:
    -Fixing apiPut test case
    -Removing comments from test cases.
    
    Also, adding spaces and break lines to test-apiGet-failure.R
    
    RSDK-125: pfsrsdk error handling on HTTP function
    
    Changing error handling of http functions to:
    -Return object with PFS error informations.
    -Dont stop execution of functions.
    -Move tests from separate files to test-httpFunctions.R
    -Alter tests for Experiment publish and unpublish to expect warnings instead of errors.
    
    RSDK-125: pfsrsdk error handling on HTTP functions
    Adding a warning when the error was not generated by PFS APIs.
    
    RSDK-125: pfsrsdk error handling on HTTP functions
    -Adding my name to authors
    -Adding changes to NEWS.md
    -Applying styler to all package.
    -Analyzing error message and returning more details.
    -Modification of authBasic.
    
    Supressing warnings for opening the connection to PFS.
    
    set warn option to 2 to test raising warnings as errors for gradle to exit.
    
    corrected spacing

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,7 +2,7 @@
 results <- file("Results.Rout", open = "wt")
 sink(results, type = "message")
 api <- coreAPI(env$auth)
-con <- authBasic(api, useVerbose = verbose)
+con <- suppressWarnings(authBasic(api, useVerbose = verbose))
 data <- jsonlite::fromJSON(env$data)
 # configure test environment for JSON SDK usage
 pfsrsdk:::loadXmlConfigFile(con$coreApi, appName = "core_CoreApp.SDK")
