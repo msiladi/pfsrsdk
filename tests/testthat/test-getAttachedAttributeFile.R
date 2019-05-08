057d55fb854bc1710ee203463bcd17ab71c7b0a8
natashamora <natasha.mora@thermofisher.com>
2019-05-08 13:34:02 -0400

    Addressed RSDK-101. Updated getAttachedAttributeFile and test to use OData call instead of HTTR call. Edited apiGet to include the useRaw parameter.
    	# Please enter the commit message for your changes. Lines starting

--- tests/testthat/test-getAttachedAttributeFile.R
+++ tests/testthat/test-getAttachedAttributeFile.R
@@ -1,11 +1,14 @@
 #' @author Adam Wheeler adam.wheeler@thermofisher.com
+#' @author Natasha Mora natasha.mora@thermofisher.com
 #' @description Tests for getAttachedAttributeFile
-#'
+
 context("Tests for getAttachedAttributeFile()")
 
 test_that(paste("test getAttachedAttributeFile() OData call on:", env$auth), {
   barcode <- getEntityByName(con$coreApi, data$testPocoType, data$testPocoName, FALSE, FALSE)$entity[[1]]$Barcode
 
   t <- getAttachedAttributeFile(con$coreApi, data$testPocoType, barcode, data$testPocoFileAttrName, useVerbose = TRUE)
-  expect_true(grepl("x", rawToChar(t$entity)))
+
+  expect_equal(t$response$status_code, 200)
+  expect_gt(length(t$entity), 0)
 })
